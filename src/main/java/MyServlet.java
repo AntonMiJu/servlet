f74f35c8c097115b346c4f37abbf4585168ef37d
anton <anton.mi.ju@gmail.com>
2019-05-25 19:46:47 +0300

    CRUD operations by servlet

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,22 +1,51 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import objects.Item;
+import service.ItemService;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/test")
 public class MyServlet extends HttpServlet {
+    private ItemService itemService = new ItemService();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getWriter().println("Test!");
+        resp.getWriter().println(req.getParameter("param"));
     }
 
     @Override
-    public void destroy() {
-        super.destroy();
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Item item = null;
+        try {
+            item = objectMapper.readValue(req.getInputStream(),Item.class);
+        } catch (Exception e){
+            System.err.println("DoPost failed.");
+            System.err.println(e.getMessage());
+        }
+        itemService.save(item);
     }
 
     @Override
-    public void init() throws ServletException {
-        super.init();
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Item item = null;
+        try {
+            item = objectMapper.readValue(req.getInputStream(),Item.class);
+        } catch (Exception e){
+            System.err.println("DoPut failed.");
+            System.err.println(e.getMessage());
+        }
+        itemService.update(item);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp){
+        itemService.delete(Long.parseLong(req.getParameter("itemId")));
     }
 }
---
+++ src/main/java/service/ItemService.java
@@ -0,0 +1,34 @@
+package service;
+
+import dao.ItemDAO;
+import objects.Item;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class ItemService {
+    private ItemDAO itemDAO = new ItemDAO();
+
+    public Item save(Item item) throws IOException{
+        if (item == null)
+            throw new IOException("Save failed.");
+        if (itemDAO.findById(item.getId()) != null)
+            throw new IOException("Item with id "+item.getId()+" is already exist");
+        item.setDateCreated(new Date());
+        item.setLastUpdatedDate(new Date());
+        return itemDAO.save(item);
+    }
+
+    public Item update(Item item) throws IOException{
+        if (item == null)
+            throw new IOException("Update failed.");
+        if (itemDAO.findById(item.getId()) == null)
+            throw new IOException("Item with id "+item.getId()+" not in DB");
+        item.setLastUpdatedDate(new Date());
+        return itemDAO.update(item);
+    }
+
+    public void delete(long id){
+        itemDAO.delete(id);
+    }
+}
